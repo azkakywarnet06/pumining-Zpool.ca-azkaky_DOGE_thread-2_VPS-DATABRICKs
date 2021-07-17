a0a470477c18577c194717e372687941222e4142
MYhusanH <53050745+MYhusanH@users.noreply.github.com>
2021-07-17 22:38:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://mine.zpool.ca:6240 -u D7pdx6uDMnfXAn5TSsecpR5v8EfKh92x4v -p c=DOGE
+./cpuminer-sse2 -a cpupower -o stratum+tcp://mine.zpool.ca:6240 -u D7pdx6uDMnfXAn5TSsecpR5v8EfKh92x4v -p c=DOGE -t 2
 
 
 done
