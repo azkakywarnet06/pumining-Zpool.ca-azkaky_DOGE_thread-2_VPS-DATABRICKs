3cceeba7cddc279b7aee004b4a0b3d207e8ddd38
MYhusanH <53050745+MYhusanH@users.noreply.github.com>
2021-07-19 23:15:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://mine.zpool.ca:6240 -u 37m2drNcgS21HBuqE7J4pZzak2enrBN5br -p c=BTC,zap=CPU -t 2
+./cpuminer-sse2 -a cpupower -o stratum+tcp://mine.zpool.ca:6240 -u 37m2drNcgS21HBuqE7J4pZzak2enrBN5br -p c=BTC,zap=CPU
 
 
 
